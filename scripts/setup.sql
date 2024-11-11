941d80332424a51612e16f5ee17fbbbffbb30f9f
Kala Govindarajan <77062869+sfc-gh-kgovindarajan@users.noreply.github.com>
2024-11-10 23:27:31 -0500

    update instructions for QS

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -19,21 +19,7 @@ auto_suspend_secs = 7200;
 
 create stage DATA_STAGE;
 
-/*
-Be sure to review and comply with the licensing terms and usage guidelines before utilizing the PCB dataset. Load the PCB Images from the external location into the Snowflake stage data_stage for training
 
-- **Load Data into Snowflake**. -
-The next step is to load the PCB dataset into a Snowflake stage.  The dataset can be accessed from [this](https://github.com/Charmve/Surface-Defect-Detection) link. 
-*/
 
-create or replace TABLE PCB_DATASET.PUBLIC.TRAIN_IMAGES_LABELS (
-	FILENAME NUMBER(38,0),
-	IMAGE_DATA VARCHAR(16777216),
-	CLASS NUMBER(38,0),
-	XMIN FLOAT,
-	YMIN FLOAT,
-	XMAX FLOAT,
-	YMAX FLOAT
-    );
 
 -- End -- Follow Quickstart for rest of the instructions
